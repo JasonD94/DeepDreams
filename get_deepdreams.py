b81def258b42ed7d86cbf512941e9b9ca830ffce
Jason Downing <jason@downing.io>
2021-02-21 20:59:15 -0500

    got a list of imgs woo

--- get_deepdreams.py
+++ get_deepdreams.py
@@ -24,4 +24,35 @@ print(str(page))
 soup = BeautifulSoup(page.content, 'html.parser')
 
 # Debug what soup got
-print(soup)
+#print(soup)
+
+# Looks like I want:
+# div ddg-type="dream" -> div -> div -> img -> data-src
+# Should be a URL pointing to the dream.
+# Maybe class item will work to get the dreams?
+
+dreams = soup.find_all('div', class_='item')
+
+count=0
+
+# Now that we have all the dreams, let's make a list of the img URLs
+img_urls = []
+for dream in dreams:
+    # Debug the dreams
+    #print(dream)
+    img_html = dream.find('img', class_='light-gallery-item')
+
+    # Debug the img
+    #print(img_html)
+
+    # Ok, that worked, we have the imgs! Now just save off the data-src
+    # for each one - that's our img url :-)
+    img_url = img_html['data-src']
+    print(img_url)
+    
+    count += 1
+
+# Appears to be 24 dreams per page.
+# So we can do basic math - number of dreams I have, divided by 24
+# And that should *roughly* get me the number of pages to look at!
+print("Found %d dreams!" % count)
