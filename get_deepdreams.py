151a082f24b445f2a8a221ffab1b86c0604ece0b
Jason Downing <jason@downing.io>
2021-04-25 16:59:17 -0400

    made a 4th option to do all the various downloading options. need to break up the script a bit, i made a giant function which isnt ideal. works though, now i can print my dreams!

--- get_deepdreams.py
+++ get_deepdreams.py
@@ -60,22 +60,229 @@ payload = {'email': '',
 # Try to post the payload to deep dream generator so we can log in
 s = session.post(login_url, data=payload, headers = dict(referer=login_url))
 
+""""
+    TODO: break this out into multiple functions, so it isn't so giant
+
+    Basically, this will get all the dreams at a given deepdreamgenerator URL.
+    You can do:
+
+    /        ==> latest dreams by default
+    /best    ==> best dreams, sorted by the number of likes on DDG
+    /account ==> latest dreams but also those that aren't public
+"""
+
+def get_deep_dreams():
+    print("Dream url is: %s" % dream_url)
+
+    # Step 1: Get the first dream page, determine how many dreams I have
+    dream_page = session.get(dream_url)
+
+    # Debug what we got - should say <Response [200]> if successful
+    print(str(dream_page))
+
+    # Step 2: Use BeautifulSoup 2 parse le page
+    dream_soup = BeautifulSoup(dream_page.content, 'html.parser')
+
+    # Debug what soup got
+    # This is YUGE so uncomment if you dare.
+    #print(dream_soup)
+
+    # Step 3: Get number of dreams I have
+    # Looks like it's contained in a span with the class 'counter-box'
+    counter_box = dream_soup.find('span', class_='counter-box')
+
+    print(counter_box)
+
+    # Check for no counter_box; this is a tell tale sign we're stuck at the log in page for DDG...
+    # If so, the safest option to warn the user & exit, instead of causing an exception on the next line.
+    if counter_box is None:
+        print("ERROR: log in to Deep Dream Generator Failed! Check your creds or maybe this script borked. :(")
+
+        # Ye this is apparently not recommended but it doesn't quit the Idle shell on my Windows box so...
+        raise SystemExit
+
+    number_of_dreams = int(counter_box.text)
+
+    # If that worked, I should see '214' print as of 2/21/2021 9pm
+    print("Number of dreams I has: %d" % number_of_dreams)
+
+    # Divide number of dreams by 24, AND ALWAYS round up [ hence math.ceil() ]
+    # Since we want 9.1 to turn into Page 10, not Page 9
+    number_of_pages = math.ceil(number_of_dreams / 24)
+    print("We should parse %d pages I thinks...\n" % number_of_pages)
+
+    # For sorting_type "3", we'll have to grab the number of pages from the
+    # "pagination" ul class. Note that these can be spans or hrefs! so don't
+    # be too specific to beautifulsoup.
+    pagination_ul_list = dream_soup.find_all(class_='page-link')
+    max_page = 0
+
+    #print(pagination_ul_list)
+
+    # Get the largest page number.
+    for pagination_element in pagination_ul_list:
+        if pagination_element is not None:
+            try:
+                if int(pagination_element.text) > max_page:
+                    max_page = int(pagination_element.text)
+            except:
+                # A few of the pagination elements aren't valid integers
+                # Examples: < ... >
+                # So just ignore them.
+                #print("Invalid page number found for %s" % pagination_element)
+                continue
+
+    # Debugging
+    print("Looks like the number of pages should be %d" % max_page)
+
+    # Only use max_page if we're downloading ALL of my dreams
+    # Otherwise we'll stick to the tried & true counter_box
+    if sorting_type is "3":
+        number_of_pages = max_page
+
+    # Counter to make sure we got the right amount of dream urls
+    real_number_of_dreams = 0
+
+    # List of all the dream img urls for mass downloading later on
+    img_urls = []
+
+    # Now we can loop over all the deep dream pages.
+    # We have the number of pages from parsing the first page.
+    # Number of pages is currently 9, so this loop should go pages 1 to 9
+    # +1 because range doesn't include the end value
+    for page_num in range (1, int(number_of_pages) + 1):
+
+        # I deleted the copy/pasta code, so now this loop handles all dreams
+        # Thus, must handle the edge case of first page not having a ?page=
+        # in the dream page URL
+        cur_dream_url = ''
+        if page_num == 1:
+            cur_dream_url = dream_url
+        else:   
+            cur_dream_url = dream_url + "?page=" + str(page_num)
+
+        print("cur_dream_url is: %s" % cur_dream_url)
+
+        # Step 1: Get the dream page
+        # Step 2: Use BeautifulSoup 2 parse the dream page
+        # Step 3: Get list of dream
+        # NOTE: probably fine using requests here, but if that breaks, switch to 'session' instead
+        dream_page = session.get(cur_dream_url)
+        dream_soup = BeautifulSoup(dream_page.content, 'html.parser')
+        dreams = dream_soup.find_all('div', class_='item')
+
+        # Counter to monitor number of dreams per page
+        # Testing shows max of 24 dreams per page, but could be less due to incomplete pages
+        dream_count=0
+
+        # Step 4: Get list of dream img URLs
+        for dream in dreams:
+
+            # Find the img's HTML
+            img_html = dream.find('img', class_='light-gallery-item')
+
+            # Ok, that worked, we have the imgs! Now just save off the data-src
+            # for each one - that's our img url :-)
+            img_url = img_html['data-src']
+            img_urls.append(img_url)
+            
+            dream_count += 1
+            real_number_of_dreams += 1
+
+        # At this point, we're done for the current page.
+        # Let's check how many dreams we got for the page though
+        print("Found %d dreams for page %d!" % (dream_count, page_num))
+
+        # Debugging - add a break here if things break, so you don't go through
+        # all the pages just to find out something broke with the img downloading code.
+        #break;
+
+    # Some debugging checks
+    # Should have gotten the same number of dreams as seen previously
+    # So number_of_dreams should equal real_number_of_dreams
+    print("\nFound %d number of 'real' dreams" % number_of_dreams)
+    print("We previously thought there would be %d dreams" % real_number_of_dreams)
+
+    # Debugging: does our list contain the same number of URLs?
+    print("img_urls contains %d img urls\n" % len(img_urls))
+
+    # Now that we have a list of img URLs, download them to an img directory
+    # First, make sure an "img" directory exists
+    downloads_dir = "latest_dreams"
+
+    # Using a separate dir for best dream sorting download
+    if sorting_type is "2":
+        downloads_dir = "best_dreams"
+
+    elif sorting_type is "3":
+        downloads_dir = "all_dreams"
+
+    does_dir_exist = os.path.isdir(downloads_dir)
+
+    if does_dir_exist is False:
+        print("Making the %s directory..." % downloads_dir)
+        os.mkdir(downloads_dir)
+    else:
+        print("%s directory already exists :)" % downloads_dir)
+
+    dream_count=1
+    dream_downloaded=1
+    dream_errors=0
+
+    # Second, download all the images
+    for img_url in img_urls:
+        name = os.path.basename(img_url)
+        filename = os.path.join(downloads_dir, name)
+
+        # If sorting by best, then filename should be "dream_num###.jpg"
+        # instead of whatever randomly generated filename DeepDreamGenerator uses
+        if sorting_type is "2":
+            name = "dream_num" + str(dream_count) + ".jpg"
+            filename = os.path.join(downloads_dir, name)
+
+        if not os.path.isfile(filename):
+            print("Downloading: %s to %s" % (name, filename))
+            
+            try:
+                urllib.request.urlretrieve(img_url, filename)
+                dream_downloaded += 1
+                
+            except Exception as exception:
+                print(exception)
+                print("Encountered unknown error when trying to download %s. Continuing." % filename)
+                dream_errors += 1
+        else:
+            print("%s already downloaded!" % filename)
+
+        dream_count += 1
+
+    # At this point, we should have all the dreams nicely downloaded
+    # Since we skip dreams already downloaded, this shouldn't take long to run
+    # after publishing new dreams. niace!
+    print("\nFound %d dreams. Downloaded %d of them this run.\n" % (dream_count, dream_downloaded))
+    print("NOTE: found %d errors when trying to download dreams.\n" % dream_errors)
+
+    print("Hopefully all your dreams have come true!")
+
+
 # Step 0: Do we want to download dreams by latest or best sorting? Or all the dreams even non-public ones?
 # If we pick latest, we should name the dream based on the DDG file name
 # If we pick best, we should name the dreams like "dream_num1.jpg", "dream_num2",
 # etc where 1 == #1 dream, 2 == #2 dream, etc.
 # If we pick all, then we should just name dreams based on the DDG file name
 print("¡¡¡¡ WARNING: only download dreams you personally created !!!!\n\n")
-print("** Deep Dream Generator Downloader V1.0 **")
-print("Enter 1 for latest dream sorting, 2 for best dream sorting or 3 for all dreams: ")
+print("** Deep Dream Generator Downloader V1.1 **")
+print("Enter 1 for latest dream sorting, 2 for best dream sorting, 3 for all dreams and 4 for all of these: ")
 sorting_type = ""
 
-while sorting_type is not "1" and sorting_type is not "2" and sorting_type is not "3":
+while sorting_type is not "1" and sorting_type is not "2" and sorting_type is not "3" and sorting_type is not "4":
     sorting_type = input()
 
-    if sorting_type is not "1" and sorting_type is not "2" and sorting_type is not "3":
+    if sorting_type is not "1" and sorting_type is not "2" and sorting_type is not "3" \
+       and sorting_type is not "4" :
         print("\nError: invalid sorting type. ")
-        print("Enter 1 for latest dream sorting, 2 for best dream sorting or 3 for all dreams: ")
+        print("Enter 1 for latest dream sorting, 2 for best dream sorting, 3 for all dreams \
+               and 4 for all of these: ")
 
 if sorting_type is "2":
     dream_url = dream_url + "/best"
@@ -83,194 +290,23 @@ if sorting_type is "2":
 elif sorting_type is "3":
     dream_url = dream_url + "/account"
 
-print("Dream url is: %s" % dream_url)
+elif sorting_type is "4":
 
-# Step 1: Get the first dream page, determine how many dreams I have
-dream_page = session.get(dream_url)
+    # Method 1 first, "latest" sorting
+    dream_url = "https://deepdreamgenerator.com/u/" + username
+    get_deep_dreams()
 
-# Debug what we got - should say <Response [200]> if successful
-print(str(dream_page))
+    # Method 2 second, "best" sorting
+    dream_url = "https://deepdreamgenerator.com/u/" + username + "/best"
+    get_deep_dreams()
 
-# Step 2: Use BeautifulSoup 2 parse le page
-dream_soup = BeautifulSoup(dream_page.content, 'html.parser')
+    # Finally Method 3, "all dreams even though not public"
+    dream_url = "https://deepdreamgenerator.com/u/" + username + "/account"
+    get_deep_dreams()
 
-# Debug what soup got
-# This is YUGE so uncomment if you dare.
-#print(dream_soup)
+    quit()  # Fin
 
-# Step 3: Get number of dreams I have
-# Looks like it's contained in a span with the class 'counter-box'
-counter_box = dream_soup.find('span', class_='counter-box')
 
-print(counter_box)
+# If we end up here, then options 1 - 3 were selected. So grab the requested dreams!
+get_deep_dreams()
 
-# Check for no counter_box; this is a tell tale sign we're stuck at the log in page for DDG...
-# If so, the safest option to warn the user & exit, instead of causing an exception on the next line.
-if counter_box is None:
-    print("ERROR: log in to Deep Dream Generator Failed! Check your creds or maybe this script borked. :(")
-
-    # Ye this is apparently not recommended but it doesn't quit the Idle shell on my Windows box so...
-    raise SystemExit
-
-number_of_dreams = int(counter_box.text)
-
-# If that worked, I should see '214' print as of 2/21/2021 9pm
-print("Number of dreams I has: %d" % number_of_dreams)
-
-# Divide number of dreams by 24, AND ALWAYS round up [ hence math.ceil() ]
-# Since we want 9.1 to turn into Page 10, not Page 9
-number_of_pages = math.ceil(number_of_dreams / 24)
-print("We should parse %d pages I thinks...\n" % number_of_pages)
-
-# For sorting_type "3", we'll have to grab the number of pages from the
-# "pagination" ul class. Note that these can be spans or hrefs! so don't
-# be too specific to beautifulsoup.
-pagination_ul_list = dream_soup.find_all(class_='page-link')
-max_page = 0
-
-#print(pagination_ul_list)
-
-# Get the largest page number.
-for pagination_element in pagination_ul_list:
-    if pagination_element is not None:
-        try:
-            if int(pagination_element.text) > max_page:
-                max_page = int(pagination_element.text)
-        except:
-            # A few of the pagination elements aren't valid integers
-            # Examples: < ... >
-            # So just ignore them.
-            #print("Invalid page number found for %s" % pagination_element)
-            continue
-
-# Debugging
-print("Looks like the number of pages should be %d" % max_page)
-
-# Only use max_page if we're downloading ALL of my dreams
-# Otherwise we'll stick to the tried & true counter_box
-if sorting_type is "3":
-    number_of_pages = max_page
-
-# Counter to make sure we got the right amount of dream urls
-real_number_of_dreams = 0
-
-# List of all the dream img urls for mass downloading later on
-img_urls = []
-
-# Now we can loop over all the deep dream pages.
-# We have the number of pages from parsing the first page.
-# Number of pages is currently 9, so this loop should go pages 1 to 9
-# +1 because range doesn't include the end value
-for page_num in range (1, int(number_of_pages) + 1):
-
-    # I deleted the copy/pasta code, so now this loop handles all dreams
-    # Thus, must handle the edge case of first page not having a ?page=
-    # in the dream page URL
-    cur_dream_url = ''
-    if page_num == 1:
-        cur_dream_url = dream_url
-    else:   
-        cur_dream_url = dream_url + "?page=" + str(page_num)
-
-    print("cur_dream_url is: %s" % cur_dream_url)
-
-    # Step 1: Get the dream page
-    # Step 2: Use BeautifulSoup 2 parse the dream page
-    # Step 3: Get list of dream
-    # NOTE: probably fine using requests here, but if that breaks, switch to 'session' instead
-    dream_page = session.get(cur_dream_url)
-    dream_soup = BeautifulSoup(dream_page.content, 'html.parser')
-    dreams = dream_soup.find_all('div', class_='item')
-
-    # Counter to monitor number of dreams per page
-    # Testing shows max of 24 dreams per page, but could be less due to incomplete pages
-    dream_count=0
-
-    # Step 4: Get list of dream img URLs
-    for dream in dreams:
-
-	# Find the img's HTML
-        img_html = dream.find('img', class_='light-gallery-item')
-
-        # Ok, that worked, we have the imgs! Now just save off the data-src
-        # for each one - that's our img url :-)
-        img_url = img_html['data-src']
-        img_urls.append(img_url)
-        
-        dream_count += 1
-        real_number_of_dreams += 1
-
-    # At this point, we're done for the current page.
-    # Let's check how many dreams we got for the page though
-    print("Found %d dreams for page %d!" % (dream_count, page_num))
-
-    # Debugging - add a break here if things break, so you don't go through
-    # all the pages just to find out something broke with the img downloading code.
-    #break;
-
-# Some debugging checks
-# Should have gotten the same number of dreams as seen previously
-# So number_of_dreams should equal real_number_of_dreams
-print("\nFound %d number of 'real' dreams" % number_of_dreams)
-print("We previously thought there would be %d dreams" % real_number_of_dreams)
-
-# Debugging: does our list contain the same number of URLs?
-print("img_urls contains %d img urls\n" % len(img_urls))
-
-# Now that we have a list of img URLs, download them to an img directory
-# First, make sure an "img" directory exists
-downloads_dir = "latest_dreams"
-
-# Using a separate dir for best dream sorting download
-if sorting_type is "2":
-    downloads_dir = "best_dreams"
-
-elif sorting_type is "3":
-    downloads_dir = "all_dreams"
-
-does_dir_exist = os.path.isdir(downloads_dir)
-
-if does_dir_exist is False:
-    print("Making the %s directory..." % downloads_dir)
-    os.mkdir(downloads_dir)
-else:
-    print("%s directory already exists :)" % downloads_dir)
-
-dream_count=1
-dream_downloaded=1
-dream_errors=0
-
-# Second, download all the images
-for img_url in img_urls:
-    name = os.path.basename(img_url)
-    filename = os.path.join(downloads_dir, name)
-
-    # If sorting by best, then filename should be "dream_num###.jpg"
-    # instead of whatever randomly generated filename DeepDreamGenerator uses
-    if sorting_type is "2":
-        name = "dream_num" + str(dream_count) + ".jpg"
-        filename = os.path.join(downloads_dir, name)
-
-    if not os.path.isfile(filename):
-        print("Downloading: %s to %s" % (name, filename))
-        
-        try:
-            urllib.request.urlretrieve(img_url, filename)
-            dream_downloaded += 1
-            
-        except Exception as exception:
-            print(exception)
-            print("Encountered unknown error when trying to download %s. Continuing." % filename)
-            dream_errors += 1
-    else:
-        print("%s already downloaded!" % filename)
-
-    dream_count += 1
-
-# At this point, we should have all the dreams nicely downloaded
-# Since we skip dreams already downloaded, this shouldn't take long to run
-# after publishing new dreams. niace!
-print("\nFound %d dreams. Downloaded %d of them this run.\n" % (dream_count, dream_downloaded))
-print("NOTE: found %d errors when trying to download dreams.\n" % dream_errors)
-
-print("Hopefully all your dreams have come true!")
